add1a1a837c8fa77396d03134e4f41f4593ee42d
Pompeiro <pompeiro10@wp.pl>
2021-02-08 08:51:10 +0100

    Added images and slide_from_arrow_to_ring()

--- ring.py
+++ ring.py
@@ -4,7 +4,26 @@ Created on Mon Feb  8 08:18:41 2021
 
 @author: janusz
 """
+import pyautogui
+import keyboard
+
+RING_SLOT = (1760,310)
+ARROW_SLOT = (1840,310)
+
+while True:
+    if keyboard.is_pressed("p"):
+        print("You pressed p")
+        break
+
+def slide_from_arrow_to_ring():
+    if keyboard.is_pressed("p"):
+        pyautogui.moveTo(ARROW_SLOT)
+        pyautogui.mouseDown()
+        pyautogui.moveTo(RING_SLOT)
+        pyautogui.mouseUp()
+
 def main():
-    print("f")
+    slide_from_arrow_to_ring()
+
 if __name__ == "__main__":
     main()
